ae42bfc74dd8b1454c02b190c5d42dde1be56dfa
Martin <mkmartinoes@gmail.com>
2022-10-27 16:24:46 +0300

    ft: project show method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,11 @@
 class ProjectsController < ApplicationController
 
   def index
-    render json: @current_user.organizations.projects, status: :ok
+    render json: @current_user.organization.projects, status: :ok
+  end
+
+  def show
+    project = Project.find_by_id(params[:id])
+    render json: project , status: :ok
   end
 end
